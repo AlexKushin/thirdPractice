57745276b0f66df1c10400b3f0fe9c26b89524ba
AlexKushin <kyshin1993@gmail.com>
2023-03-24 02:29:48 +0100

    exceptions added

---
+++ src/main/java/com/shpp/mentoring/okushin/exceptions/NotInputtedFormatException.java
@@ -0,0 +1,7 @@
+package com.shpp.mentoring.okushin.exceptions;
+
+public class NotInputtedFormatException extends RuntimeException{
+    public NotInputtedFormatException(String message){
+        super(message);
+    }
+}
