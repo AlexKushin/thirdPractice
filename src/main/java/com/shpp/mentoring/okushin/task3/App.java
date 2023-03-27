0b37a97441e437924cbb56da211864add70b9b33
AlexKushin <kyshin1993@gmail.com>
2023-03-27 14:30:41 +0200

    comment for checking added

--- src/main/java/com/shpp/mentoring/okushin/task3/App.java
+++ src/main/java/com/shpp/mentoring/okushin/task3/App.java
@@ -16,7 +16,7 @@ public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-
+//some changes
         int messagesAmount;
         if (args.length != 0) {
             try {
